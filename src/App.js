f0026d11bdc46447da4640392c414cc8f5030940
Brandon H. Goding <brandon.h.goding@gmail.com>
2024-08-01 20:23:25 -0400

    feature(blog-router): setting up blog router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import NowPlaying from "./components/now-playing/NowPlaying";
 import {SwatchIcon, ClockIcon} from '@heroicons/react/20/solid'
 import Footer from "./components/footer/Footer";
 import BlogTeaser from "./components/blog_teasers/BlogTeaser";
+import BlogPost from "./components/blog_post/BlogPost";
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
 
 
 const navigation = [
@@ -57,9 +59,9 @@ const nowPlaying = [
 ]
 
 
-function App() {
+function HomePage() {
     return (
-        <div className="App">
+        <>
             <BackgroundImageHero navigation={navigation} />
             <div className="overflow-hidden bg-white py-12 sm:py-16">
                 <h2 className="text-4xl font-bold tracking-tight sm:text-6xl text-center mb-12 sm:mb-16">Now Playing at Fort Kent Cinema</h2>
@@ -67,15 +69,39 @@ function App() {
                     <div
                         className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                         {nowPlaying.map((movie) => (
-                            <NowPlaying title={movie.title} starting={movie.startDate} features={movie.features}/>
+                            <NowPlaying key={movie.title} title={movie.title} starting={movie.startDate} features={movie.features}/>
                         ))}
                     </div>
                 </div>
             </div>
-            <ShowTimes/>
+            <ShowTimes />
             <BlogTeaser />
-            <Footer navigation={navigation} />
-        </div>
+        </>
+    );
+}
+
+function BlogPage() {
+    const { postName } = useParams();
+    return (
+        <>
+            <BackgroundImageHero navigation={navigation} />
+            <BlogPost postName={postName} />
+        </>
+    );
+}
+
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/fort-kent-cinema-blog/:postName" element={<BlogPage />} />
+                </Routes>
+                <Footer navigation={navigation} />
+            </div>
+        </Router>
     );
 }
 
